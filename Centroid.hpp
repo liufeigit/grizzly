5a1484be344bd7af0279454eb402fd797cebbc6d
Milan van der Meer <vandermeer.milan@gmail.com>
2016-10-31 15:59:43 +0100

    Deleted WeightedSum and fixed Centroid
    Centroid formerly used WeightedSum

--- Centroid.hpp
+++ Centroid.hpp
@@ -9,17 +9,27 @@
 #ifndef BEAR_DSP_CENTROID_HPP
 #define BEAR_DSP_CENTROID_HPP
 
-#include <gsl/gsl.h>
+#include <gsl/span>
 
-#include "WeightedSum.hpp"
+#include "Parallel.hpp"
 
 namespace bear::dsp
 {
     //! The centroid of a span of values
+    /*! The centroid or 'center of gravity' is sum of values, weighted by its index, divided by the sum of values. */
     template <typename T, long N>
     inline static float centroid(gsl::span<T, N> values)
     {
-        return indexWeightedSum<T>(values) / sum(values);
+        // Accumulator
+        T indexWeightedSum = 0;
+        
+        // Multiply each value with its index number and accumulate
+        for (auto index = 0; index < values.size(); ++index)
+        {
+            indexWeightedSum += index * values[index];
+        }
+        
+        return indexWeightedSum / sum<T>(values);
     }
 }
 
