d269769855a38a5c4b5e102f906d6a9de4bb85b5
Yuri Wilmering <yuri.wilmering@student-kmt.hku.nl>
2016-11-08 12:29:29 +0100

    some minor optimalizations

--- SchroederReverb.hpp
+++ SchroederReverb.hpp
@@ -4,6 +4,10 @@
 //  Created by Yuri Wilmering on 07/11/16.
 //  Copyright © 2016 Yuri Wilmering. All rights reserved.
 //
+
+#ifndef BEAR_DSP_REVERB_HPP
+#define BEAR_DSP_REVERB_HPP
+
 #include <vector>
 
 #include "AllPass.hpp"
@@ -11,8 +15,7 @@
 using namespace bear;
 using namespace std;
 
-#ifndef BEAR_DSP_REVERB_HPP
-#define BEAR_DSP_REVERB_HPP
+#include <iostream>
 
 namespace bear::dsp
 {
@@ -25,26 +28,24 @@ namespace bear::dsp
             gain(gain),
             maxNrofFilters(maxNrofFilters)
         {
-            while (delayTime >= 1 && nrofFilters < maxNrofFilters)
+            while (delayTime >= 1 && allPass.size() < maxNrofFilters)
             {
                 allPass.emplace_back(delayTime, gain);
                 delayTime /= 3;
-                nrofFilters++;
             }
         }
 
         T process(const T& x) final override
         {
-            _x = x;
+            auto y = x;
 
-            for (auto filter = 0; filter < nrofFilters; filter++)
-            {
-                y = allPass[filter](_x);
-                _x = y;
-            }
+            for (auto& filter : allPass)
+                y = filter(y);
 
             return y;
         }
+
+        auto getFilterCount() const { return allPass.size(); }
 
     public:
         float gain = 0;
@@ -52,10 +53,7 @@ namespace bear::dsp
         int delayTime = 0;
 
     private:
-        vector<dsp::AllPassFilter<float>> allPass;
-        int nrofFilters = 0;
-        float _x = 0;
-        float y = 0;
+        std::vector<dsp::AllPassFilter<float>> allPass;
     };
 }
 
