f07613f2caddd85eb47b5cd83f04c740c83f1fb7
Stijn Frishert <info@stijnfrishert.com>
2016-11-22 16:06:04 +0100

    More tests for Ramp

--- test/Ramp.cpp
+++ test/Ramp.cpp
@@ -5,11 +5,34 @@
 using namespace dsp;
 using namespace std;
 
-TEST_CASE("test Ramp.hpp")
+TEST_CASE("Ramp")
 {
+    SUBCASE("Ramp(phase)")
+    {
+        CHECK(Ramp<float>(0.2).getPhase() == doctest::Approx(0.2));
+        CHECK(Ramp<float>(0.8).getPhase() == doctest::Approx(0.8));
+        CHECK(Ramp<float>(1.2).getPhase() == doctest::Approx(0.2));
+        CHECK(Ramp<float>(-0.2).getPhase() == doctest::Approx(0.8));
+    }
+
     Ramp<float> ramp;
 
-    SUBCASE("In Bounds")
+    SUBCASE("setPhase()")
+    {
+        ramp.setPhase(0.2);
+        CHECK(ramp.getPhase() == doctest::Approx(0.2));
+
+        ramp.setPhase(0.8);
+        CHECK(ramp.getPhase() == doctest::Approx(0.8));
+
+        ramp.setPhase(1.2);
+        CHECK(ramp.getPhase() == doctest::Approx(0.2));
+
+        ramp.setPhase(-0.2);
+        CHECK(ramp.getPhase() == doctest::Approx(0.8));
+    }
+
+    SUBCASE("increment()")
     {
         for (auto i = 0; i < 10; ++i)
         {
@@ -20,7 +43,7 @@ TEST_CASE("test Ramp.hpp")
         }
     }
     
-    SUBCASE("End function called")
+    SUBCASE("end")
     {
         auto flag = false;
         ramp.end = [&](){ flag = true; };
