3cf92b7a1e6ccfe2b7f63904b40df68b6ce10560
Stijn Frishert <info@stijnfrishert.com>
2016-11-22 18:13:14 +0100

    Rest of the waveform unit tests

--- Waveform.hpp
+++ Waveform.hpp
@@ -14,53 +14,61 @@
 
 namespace dsp
 {
-    //! Generate a bipolar sine wave given a normalised phase
+    //! Generate a bipolar sine wave given a normalized phase
     template <typename T, typename Phase>
     constexpr T generateSine(Phase phase)
     {
         return std::sin(math::TWO_PI<T> * phase);
     }
 
-    //! Generate a unipolar sine wave given a normalised phase
+    //! Generate a unipolar sine wave given a normalized phase
     template <typename T, typename Phase>
     constexpr T generateUnipolarSine(Phase phase)
     {
         return generateSine<T>(phase) * 0.5 + 0.5;
     }
     
-    //! Generate a bipolar saw wave given a normalised phase
+    //! Generate a bipolar saw wave given a normalized phase
     template <typename T, typename Phase>
     constexpr T generateSaw(Phase phase)
     {
         return math::wrap<std::common_type_t<Phase, T>>(phase + 0.5, 0, 1) * 2 - 1;
     }
     
-    //! Generate a bipolar square wave given a normalised phase
+    //! Generate a unipolar saw wave given a normalized phase
+    template <typename T, typename Phase>
+    constexpr T generateUnipolarSaw(Phase phase)
+    {
+        return math::wrap<std::common_type_t<Phase, T>>(phase, 0, 1);
+    }
+    
+    //! Generate a bipolar square wave given a normalized phase
     template <typename T, typename Phase, typename PulseWidth>
     constexpr T generateSquare(Phase phase, PulseWidth pulseWidth)
     {
         return math::wrap<Phase>(phase, 0, 1) < pulseWidth ? 1 : -1;
     }
     
-    //! Generate a unipolar square wave given a normalised phase
+    //! Generate a unipolar square wave given a normalized phase
     template <typename T1, typename T2>
     constexpr T1 generateUnipolarSquare(T1 phase, T2 pulseWidth)
     {
         return math::wrap<T1>(phase, 0, 1) < pulseWidth ? 1 : 0;
     }
     
-    //! Generate a bipolar triangle wave given a normalised phase
-    template <typename T>
-    constexpr T generateTriangle(T phase)
+    //! Generate a bipolar triangle wave given a normalized phase
+    template <typename T, typename Phase>
+    constexpr T generateTriangle(Phase phase)
     {
-        return 2 * std::fabs(2 * math::wrap<T>(phase - 0.25, 0, 1) - 1) - 1;
+        return 2 * std::fabs(2 * math::wrap<std::common_type_t<Phase, T>>(phase - 0.25, 0, 1) - 1) - 1;
     }
     
-    //! Generate a unipolar triangle wave given a normalised phase
-    template <typename T>
-    constexpr T generateUnipolarTriangle(T phase)
+    //! Generate a unipolar triangle wave given a normalized phase
+    template <typename T, typename Phase>
+    constexpr T generateUnipolarTriangle(Phase phase)
     {
-        return math::wrap<T>(phase, 0, 1) < 0.5 ? phase * 2 : (0.5 - (phase - 0.5)) * 2;
+        phase = math::wrap<Phase>(phase, 0, 1);
+        return phase < 0.5 ? phase * 2 : (0.5 - (phase - 0.5)) * 2;
     }
 }
 
--- test/Waveform.cpp
+++ test/Waveform.cpp
@@ -9,47 +9,101 @@ TEST_CASE("Waveform")
 {
     SUBCASE("sine")
     {
-        CHECK(generateSine<float>(-0.25) == doctest::Approx(-1));
-        CHECK(generateSine<float>(0) == doctest::Approx(0));
-        CHECK(generateSine<float>(0.125) == doctest::Approx(0.7071067812));
-        CHECK(generateSine<float>(0.25) == doctest::Approx(1));
-        CHECK(generateSine<float>(0.375) == doctest::Approx(0.7071067812));
-        CHECK(generateSine<float>(0.5) == doctest::Approx(0));
-        CHECK(generateSine<float>(0.625) == doctest::Approx(-0.7071067812));
-        CHECK(generateSine<float>(0.75) == doctest::Approx(-1));
-        CHECK(generateSine<float>(0.875) == doctest::Approx(-0.7071067812));
-        CHECK(generateSine<float>(1) == doctest::Approx(0));
-        CHECK(generateSine<float>(1.25) == doctest::Approx(1));
-    }
+        SUBCASE("bipolar")
+        {
+            CHECK(generateSine<float>(-0.25) == doctest::Approx(-1));
+            CHECK(generateSine<float>(0) == doctest::Approx(0));
+            CHECK(generateSine<float>(0.125) == doctest::Approx(0.7071067812));
+            CHECK(generateSine<float>(0.25) == doctest::Approx(1));
+            CHECK(generateSine<float>(0.375) == doctest::Approx(0.7071067812));
+            CHECK(generateSine<float>(0.5) == doctest::Approx(0));
+            CHECK(generateSine<float>(0.625) == doctest::Approx(-0.7071067812));
+            CHECK(generateSine<float>(0.75) == doctest::Approx(-1));
+            CHECK(generateSine<float>(0.875) == doctest::Approx(-0.7071067812));
+            CHECK(generateSine<float>(1) == doctest::Approx(0));
+            CHECK(generateSine<float>(1.25) == doctest::Approx(1));
+        }
 
-    SUBCASE("unipolar sine")
-    {
-        CHECK(generateUnipolarSine<float>(-0.25) == doctest::Approx(0));
-        CHECK(generateUnipolarSine<float>(0) == doctest::Approx(0.5));
-        CHECK(generateUnipolarSine<float>(0.125) == doctest::Approx(0.85355339059328));
-        CHECK(generateUnipolarSine<float>(0.25) == doctest::Approx(1));
-        CHECK(generateUnipolarSine<float>(0.375) == doctest::Approx(0.85355339059328));
-        CHECK(generateUnipolarSine<float>(0.5) == doctest::Approx(0.5));
-        CHECK(generateUnipolarSine<float>(0.625) == doctest::Approx(0.14644660940672));
-        CHECK(generateUnipolarSine<float>(0.75) == doctest::Approx(0));
-        CHECK(generateUnipolarSine<float>(0.875) == doctest::Approx(0.14644660940672));
-        CHECK(generateUnipolarSine<float>(1) == doctest::Approx(0.5));
-        CHECK(generateUnipolarSine<float>(1.25) == doctest::Approx(1));
+        SUBCASE("unipolar")
+        {
+            CHECK(generateUnipolarSine<float>(-0.25) == doctest::Approx(0));
+            CHECK(generateUnipolarSine<float>(0) == doctest::Approx(0.5));
+            CHECK(generateUnipolarSine<float>(0.125) == doctest::Approx(0.85355339059328));
+            CHECK(generateUnipolarSine<float>(0.25) == doctest::Approx(1));
+            CHECK(generateUnipolarSine<float>(0.375) == doctest::Approx(0.85355339059328));
+            CHECK(generateUnipolarSine<float>(0.5) == doctest::Approx(0.5));
+            CHECK(generateUnipolarSine<float>(0.625) == doctest::Approx(0.14644660940672));
+            CHECK(generateUnipolarSine<float>(0.75) == doctest::Approx(0));
+            CHECK(generateUnipolarSine<float>(0.875) == doctest::Approx(0.14644660940672));
+            CHECK(generateUnipolarSine<float>(1) == doctest::Approx(0.5));
+            CHECK(generateUnipolarSine<float>(1.25) == doctest::Approx(1));
+        }
     }
     
     SUBCASE("saw")
     {
-        CHECK(generateSaw<float>(-0.25) == doctest::Approx(-0.5));
-        CHECK(generateSaw<float>(0) == doctest::Approx(0));
-        CHECK(generateSaw<float>(0.125) == doctest::Approx(0.25));
-        CHECK(generateSaw<float>(0.25) == doctest::Approx(0.5));
-        CHECK(generateSaw<float>(0.375) == doctest::Approx(0.75));
-        CHECK(generateSaw<float>(0.5) == doctest::Approx(-1));
-        CHECK(generateSaw<float>(0.625) == doctest::Approx(-0.75));
-        CHECK(generateSaw<float>(0.75) == doctest::Approx(-0.5));
-        CHECK(generateSaw<float>(0.875) == doctest::Approx(-0.25));
-        CHECK(generateSaw<float>(1) == doctest::Approx(0));
-        CHECK(generateSaw<float>(1.25) == doctest::Approx(0.5));
+        SUBCASE("bipolar")
+        {
+            CHECK(generateSaw<float>(-0.25) == doctest::Approx(-0.5));
+            CHECK(generateSaw<float>(0) == doctest::Approx(0));
+            CHECK(generateSaw<float>(0.125) == doctest::Approx(0.25));
+            CHECK(generateSaw<float>(0.25) == doctest::Approx(0.5));
+            CHECK(generateSaw<float>(0.375) == doctest::Approx(0.75));
+            CHECK(generateSaw<float>(0.5) == doctest::Approx(-1));
+            CHECK(generateSaw<float>(0.625) == doctest::Approx(-0.75));
+            CHECK(generateSaw<float>(0.75) == doctest::Approx(-0.5));
+            CHECK(generateSaw<float>(0.875) == doctest::Approx(-0.25));
+            CHECK(generateSaw<float>(1) == doctest::Approx(0));
+            CHECK(generateSaw<float>(1.25) == doctest::Approx(0.5));
+        }
+
+        SUBCASE("unipolar")
+        {
+            CHECK(generateUnipolarSaw<float>(-0.25) == doctest::Approx(0.75));
+            CHECK(generateUnipolarSaw<float>(0) == doctest::Approx(0));
+            CHECK(generateUnipolarSaw<float>(0.125) == doctest::Approx(0.125));
+            CHECK(generateUnipolarSaw<float>(0.25) == doctest::Approx(0.25));
+            CHECK(generateUnipolarSaw<float>(0.375) == doctest::Approx(0.375));
+            CHECK(generateUnipolarSaw<float>(0.5) == doctest::Approx(0.5));
+            CHECK(generateUnipolarSaw<float>(0.625) == doctest::Approx(0.625));
+            CHECK(generateUnipolarSaw<float>(0.75) == doctest::Approx(0.75));
+            CHECK(generateUnipolarSaw<float>(0.875) == doctest::Approx(0.875));
+            CHECK(generateUnipolarSaw<float>(1) == doctest::Approx(0));
+            CHECK(generateUnipolarSaw<float>(1.25) == doctest::Approx(0.25));
+        }
+    }
+
+    SUBCASE("triangle")
+    {
+        SUBCASE("bipolar")
+        {
+            CHECK(generateTriangle<float>(-0.25) == doctest::Approx(-1));
+            CHECK(generateTriangle<float>(0) == doctest::Approx(0));
+            CHECK(generateTriangle<float>(0.125) == doctest::Approx(0.5));
+            CHECK(generateTriangle<float>(0.25) == doctest::Approx(1));
+            CHECK(generateTriangle<float>(0.375) == doctest::Approx(0.5));
+            CHECK(generateTriangle<float>(0.5) == doctest::Approx(0));
+            CHECK(generateTriangle<float>(0.625) == doctest::Approx(-0.5));
+            CHECK(generateTriangle<float>(0.75) == doctest::Approx(-1));
+            CHECK(generateTriangle<float>(0.875) == doctest::Approx(-0.5));
+            CHECK(generateTriangle<float>(1) == doctest::Approx(0));
+            CHECK(generateTriangle<float>(1.25) == doctest::Approx(1));
+        }
+
+        SUBCASE("unipolar")
+        {
+            CHECK(generateUnipolarTriangle<float>(-0.25) == doctest::Approx(0.5));
+            CHECK(generateUnipolarTriangle<float>(0) == doctest::Approx(0));
+            CHECK(generateUnipolarTriangle<float>(0.125) == doctest::Approx(0.25));
+            CHECK(generateUnipolarTriangle<float>(0.25) == doctest::Approx(0.5));
+            CHECK(generateUnipolarTriangle<float>(0.375) == doctest::Approx(0.75));
+            CHECK(generateUnipolarTriangle<float>(0.5) == doctest::Approx(1));
+            CHECK(generateUnipolarTriangle<float>(0.625) == doctest::Approx(0.75));
+            CHECK(generateUnipolarTriangle<float>(0.75) == doctest::Approx(0.5));
+            CHECK(generateUnipolarTriangle<float>(0.875) == doctest::Approx(0.25));
+            CHECK(generateUnipolarTriangle<float>(1) == doctest::Approx(0));
+            CHECK(generateUnipolarTriangle<float>(1.25) == doctest::Approx(0.5));
+        }
     }
     
     SUBCASE("square")
@@ -99,6 +153,28 @@ TEST_CASE("Waveform")
                 CHECK(generateSquare<float>(1, 1) == 1);
                 CHECK(generateSquare<float>(1.25, 1) == 1);
             }
+
+            SUBCASE("pulse width -25%")
+            {
+                CHECK(generateSquare<float>(-0.25, -0.25) == -1);
+                CHECK(generateSquare<float>(0, -0.25) == -1);
+                CHECK(generateSquare<float>(0.25, -0.25) == -1);
+                CHECK(generateSquare<float>(0.5, -0.25) == -1);
+                CHECK(generateSquare<float>(0.75, -0.25) == -1);
+                CHECK(generateSquare<float>(1, -0.25) == -1);
+                CHECK(generateSquare<float>(1.25, -0.25) == -1);
+            }
+            
+            SUBCASE("pulse width 125%")
+            {
+                CHECK(generateSquare<float>(-0.25, 1.25) == 1);
+                CHECK(generateSquare<float>(0, 1.25) == 1);
+                CHECK(generateSquare<float>(0.25, 1.25) == 1);
+                CHECK(generateSquare<float>(0.5, 1.25) == 1);
+                CHECK(generateSquare<float>(0.75, 1.25) == 1);
+                CHECK(generateSquare<float>(1, 1.25) == 1);
+                CHECK(generateSquare<float>(1.25, 1.25) == 1);
+            }
         }
         
         SUBCASE("unipolar")
@@ -145,6 +221,28 @@ TEST_CASE("Waveform")
                 CHECK(generateUnipolarSquare<float>(0.75, 1) == 1);
                 CHECK(generateUnipolarSquare<float>(1, 1) == 1);
                 CHECK(generateUnipolarSquare<float>(1.25, 1) == 1);
+            }
+
+            SUBCASE("pulse width -25%")
+            {
+                CHECK(generateUnipolarSquare<float>(-0.25, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(0, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(0.25, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(0.5, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(0.75, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(1, -0.25) == 0);
+                CHECK(generateUnipolarSquare<float>(1.25, -0.25) == 0);
+            }
+            
+            SUBCASE("pulse width 125%")
+            {
+                CHECK(generateUnipolarSquare<float>(-0.25, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(0, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(0.25, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(0.5, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(0.75, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(1, 1.25) == 1);
+                CHECK(generateUnipolarSquare<float>(1.25, 1.25) == 1);
             }
         }
     }
