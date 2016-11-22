be1050d3d52722143c98cb3e26100dce90a90342
Stijn Frishert <info@stijnfrishert.com>
2016-11-22 13:13:51 +0100

    CombFilters.hpp

---
+++ CombFilter.hpp
@@ -0,0 +1,97 @@
+//
+//  CombFilter.hpp
+//  octoSandbox1
+//
+//  Created by Vincent Pruijn on 01/11/16.
+//  Copyright © 2016 Dsperados. All rights reserved.
+//
+
+#ifndef GRIZZLY_COMB_FILTER_HPP
+#define GRIZZLY_COMB_FILTER_HPP
+
+#include "Delay.hpp"
+
+namespace dsp
+{
+    //! Feed-back Comb Filter
+    template <class T>
+    class FeedBackCombFilter
+    {
+    public:
+        FeedBackCombFilter (const std::size_t maxDelay):
+            delay(maxDelay)
+        {
+            
+        }
+        
+        //! Process function, take an input
+        T process(const T& x, float delayTime, float feedBack)
+        {
+            const auto d = delay.read(delayTime);
+            const auto y = x + feedBack * (postDelay ? postDelay(d) : d);
+            
+            delay.write(y);
+            
+            return y;
+        }
+        
+        T operator()(const T& x, float delayTime, float feedBack)
+        {
+            return process(x, delayTime, feedBack);
+        }
+
+        void setMaxDelayTime(float maxDelayTime)
+        {
+            delay.resize(maxDelayTime);
+        }
+        
+    public:
+        //! PostDelay function
+        std::function<T(const T&)> postDelay;
+        
+    private:
+        Delay<T> delay;
+    };
+    
+    //! Feed-forward Comb Filter
+    template <class T>
+    class FeedForwardCombFilter
+    {
+    public:
+        FeedForwardCombFilter (const std::size_t maxDelay):
+        delay(maxDelay)
+        {
+            
+        }
+        
+        //! Process function, take an input
+        T process(const T& x, float delayTime, float feedForward)
+        {
+            delay.write(x);
+            
+            const auto d = delay.read(delayTime);
+            const auto y = x + feedForward * (postDelay ? postDelay(d) : d);
+            
+            return y;
+        }
+        
+        T operator()(const T& x, float delayTime, float feedForward)
+        {
+            return process(x, delayTime, feedForward);
+        }
+        
+        void setMaxDelayTime(float maxDelayTime)
+        {
+            delay.resize(maxDelayTime);
+        }
+        
+        
+    public:
+        //! PostDelay function
+        std::function<T(const T&)> postDelay;
+        
+    private:
+        Delay<T> delay;
+    };
+}
+#endif /* GRIZZLY_COMB_FILTER_HPP */
