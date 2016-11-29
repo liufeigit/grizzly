6864d4d2eacbfcd5d276a1132efe84f4c4b8c1a1
Vincent Pruijn Perez <vincentpruijn@gmail.com>
2016-11-29 16:32:30 +0100

    Added FeedForwardCombFilter tests

--- test/CombFilter.cpp
+++ test/CombFilter.cpp
@@ -62,4 +62,55 @@ TEST_CASE("CombFilter")
             }
         }
     }
+    SUBCASE("FeedForwardCombFilter")
+    {
+        SUBCASE("constructor")
+        {
+            SUBCASE("size is 0")
+            {
+                FeedForwardCombFilter<int> filter = FeedForwardCombFilter<int>();
+                
+                CHECK(filter.getMaxDelayTime() == 0);
+            }
+            
+            SUBCASE("is initialized to 0")
+            {
+                FeedForwardCombFilter<int> filter (4);
+                
+                REQUIRE (filter.getMaxDelayTime() == 4);
+                
+                for (int i = 0; i < 4; i++)
+                    CHECK(filter(0., 0., 0.) == 0);
+            }
+        }
+        
+        SUBCASE("process()")
+        {
+            SUBCASE("Impulse")
+            {
+                FeedForwardCombFilter<double> filter (2);
+                
+                CHECK(filter(1., 1., 0.5) == doctest::Approx(1.));
+                CHECK(filter(0., 1., 0.5) == doctest::Approx(0.5));
+                CHECK(filter(0., 1., 0.5) == doctest::Approx(0.));
+            }
+            
+            SUBCASE("postDelay")
+            {
+                FeedForwardCombFilter<double> filter (2);
+                filter.postDelay = [&](const double& x)
+                {
+                    static double xHistory = 0.;
+                    auto sum = x + xHistory;
+                    xHistory = x;
+                    return sum / 2.;
+                };
+                
+                CHECK(filter(1., 1., 0.5) == doctest::Approx(1.0));
+                CHECK(filter(0., 1., 0.5) == doctest::Approx(0.25));
+                CHECK(filter(0., 1., 0.5) == doctest::Approx(0.25));
+                CHECK(filter(0., 1., 0.5) == doctest::Approx(0.));
+            }
+        }
+    }
 }
