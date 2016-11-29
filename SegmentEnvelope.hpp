786d4cc3791f08bc2c6e9831e5bdb284f43122b9
Milan van der Meer <vandermeer.milan@gmail.com>
2016-11-29 16:28:28 +0100

    Updated SegmentEnvelope (working but not complete)

--- SegmentEnvelope.hpp
+++ SegmentEnvelope.hpp
@@ -2,14 +2,20 @@
 #ifndef GRIZZLY_SEGMENTENVELOPE
 #define GRIZZLY_SEGMENTENVELOPE
 
+
 #include <dsperados/math/utility.hpp>
 #include <initializer_list>
 #include <dsperados/math/ease.hpp>
 #include <dsperados/math/interpolation.hpp>
+#include <experimental/optional>
 #include <functional>
+#include <map>
+#include <numeric>
 #include <unit/time.hpp>
 #include <utility>
 #include <vector>
+
+#include <iostream>
 
 namespace dsp
 {
@@ -42,6 +48,12 @@ namespace dsp
             std::function<double(double)> ease = math::easeLinear<double>;
         };
         
+        struct Hold
+        {
+            unit::second<Time> timePoint = 0;
+            bool enabled = false;
+        };
+        
     public:
         SegmentEnvelope() = default;
         
@@ -50,43 +62,76 @@ namespace dsp
         {
         }
         
+        void setState(unit::second<Time> to)
+        {
+            auto envelopeDuration = std::accumulate(segments.begin(), segments.end(), unit::second<Time>(0) , [](const unit::second<Time>& acc, const Segment& segment) { return acc + segment.duration; } );
+            
+            if (to >= envelopeDuration)
+            {
+                envelopeTime = envelopeDuration;
+                segmentTime = segments.back().duration;
+                index = segments.size();
+            }
+            else
+            {
+                envelopeTime = to;
+                
+                unit::second<Time> parialTime = 0;
+                for (auto i = 0; i < segments.size(); ++i)
+                {
+                    if (parialTime + segments[i].duration < envelopeTime)
+                    {
+                        parialTime += segments[i].duration;
+                        continue;
+                    }
+                    
+                    index = i;
+                    segmentTime = envelopeTime - parialTime;
+                    break;
+                }
+            }
+        }
         
+        void unHold()
+        {
+            if (hold)
+                hold->enabled = false;
+        }
         
         //! Increment the time
         void increment(const Time& increment)
         {
+            segmentTime += increment;
+            envelopeTime += increment;
             
-            
-            time += increment;
-            
-            while (time >= segments[index].duration && index < segments.size())
+            if (hold && hold->enabled && envelopeTime >= hold->timePoint)
             {
-                time -= segments[index].duration;
-                ++index;
-                
-                if (loopMode && index == loopPoints.second)
-                    index = loopPoints.first;
+                segmentTime -= envelopeTime - hold->timePoint;
+                envelopeTime = hold->timePoint;
             }
             
+            while (segmentTime >= segments[index].duration && index < segments.size())
+            {
+                segmentTime -= segments[index].duration;
+                ++index;
+            }
         }
         
         //! Return the current envelope value
         Value read()
         {
-            
-            
-            
             if (index >= segments.size())
                 return segments.back().destination;
             
             auto currentSegment = segments[index];
             
-            auto normalizedOutput = currentSegment.ease(time / static_cast<double>(currentSegment.duration));
+            auto normalizedOutput = currentSegment.ease(segmentTime / static_cast<double>(currentSegment.duration));
             
             return math::interpolateLinear(normalizedOutput, index == 0 ? Value(0) : segments[index - 1].destination, currentSegment.destination);
         }
         
-        
+        //! Optinal hold point
+        std::experimental::optional<Hold> hold = std::experimental::nullopt;
         
     private:
         //! Vector for Segments
@@ -95,11 +140,10 @@ namespace dsp
         //! The segment index
         size_t index = 0;
         
-        unit::second<Time> time = 0;
         
-        std::pair<size_t, size_t> loopPoints = {1, 2};
+        unit::second<Time> segmentTime = 0;
+        unit::second<Time> envelopeTime = 0;
         
-        bool loopMode = true;
     };
 }
 
